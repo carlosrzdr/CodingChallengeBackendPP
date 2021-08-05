2f34bdf7eb3ea59f3bb9db580d6f5ec330f5b17b
Carlos Ruiz <carlos.rzdr@gmail.com>
2021-08-05 18:52:23 +0200

    [Setup] Minimum Flask app #1

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,10 @@
 from flask import Flask
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(TESTING=False,
+                            DEBUG=True,
+                            SECRET_KEY='secret_key',
+                            SERVER='0.0.0.0')
+
+    return app
