107b8bce0461213e54bc51d9c78a72e5b14eb22e
Carlos Ruiz <carlosrzdr@gmail.com>
2021-08-07 19:30:56 +0200

    [Backend] Added tests for plate module #6

---
+++ tests/test_plate.py
@@ -0,0 +1,35 @@
+from app.plate import plate_is_valid, search_plate_levenshtein, row2dict, get_plates
+from app.models import Plate
+
+def test_plate_validation_correct():
+    plate_number = 'M-PP123'
+    assert plate_is_valid(plate_number) != None
+
+def test_plate_validation_error():
+    no_number_plate_number = 'M-PP'
+    start_zero_plate_number = 'M-PP0123'
+    too_many_numbers_plate_number = 'M-PP12345'
+    before_hyphen_letters_plate_number = 'MMMM-PP123'
+    after_hyphen_letters_plate_number = 'MMMM-PP123'
+
+    assert plate_is_valid(no_number_plate_number) == None
+    assert plate_is_valid(start_zero_plate_number) == None
+    assert plate_is_valid(too_many_numbers_plate_number) == None
+    assert plate_is_valid(before_hyphen_letters_plate_number) == None
+    assert plate_is_valid(after_hyphen_letters_plate_number) == None
+
+def test_search_plate_levenshtein_found():
+    expected_plate = Plate('M-PP123')
+    expected_plate.save()
+    searched_plate_levenshtein_1 = 'MM-PP123'
+    searched_plate_levenshtein_2 = 'Q-PP1234'
+
+    assert search_plate_levenshtein(searched_plate_levenshtein_1, 1) == [expected_plate]
+    assert search_plate_levenshtein(searched_plate_levenshtein_2, 2) == [expected_plate]
+
+def test_search_plate_levenshtein_not_found():
+    expected_plate = Plate('M-PP123')
+    expected_plate.save()
+    searched_plate_levenshtein = 'MMM-PP123'
+
+    assert search_plate_levenshtein(searched_plate_levenshtein, 1) == []
