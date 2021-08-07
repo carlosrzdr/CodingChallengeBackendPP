d728a73ba01db011c0d834413d1b712255d122ec
Carlos Ruiz <carlos.rzdr@gmail.com>
2021-08-08 01:52:09 +0200

    [Backend] Fix tests to use app variable declared in conftest.py #6

--- tests/test_plate_blueprint.py
+++ tests/test_plate_blueprint.py
@@ -2,9 +2,7 @@ from app.actions.plate_actions import row2dict
 from app import create_app
 from app.models import Plate
 
-def test_plate_get_request(session):
-    app = create_app(testing=True)
-
+def test_plate_get_request(session, app):
     with app.test_client() as test_client:
         expected_plate = Plate('M-PP123')
         session.add(expected_plate)
@@ -12,16 +10,12 @@ def test_plate_get_request(session):
         response = test_client.get('/plate')
         assert eval(response.data) == [row2dict(expected_plate)]
 
-def test_plate_get_request_empty(session):
-    app = create_app(testing=True)
-
+def test_plate_get_request_empty(app, session):
     with app.test_client() as test_client:
         response = test_client.get('/plate')
         assert eval(response.data) == []
 
-def test_plate_post_request_success(session):
-    app = create_app(testing=True)
-
+def test_plate_post_request_success(session, app):
     with app.test_client() as test_client:
         expected_plate = Plate('M-PP123')
         session.add(expected_plate)
@@ -29,23 +23,17 @@ def test_plate_post_request_success(session):
         response = test_client.post('/plate', data = {'plate': 'M-PP123'})
         assert response.status_code == 200
     
-def test_plate_post_request_not_valid_plate():
-    app = create_app(testing=True)
-
+def test_plate_post_request_not_valid_plate(app):
     with app.test_client() as test_client:
         response = test_client.post('/plate', data = {'plate': 'M-PPP123'})
         assert response.status_code == 422 
 
-def test_plate_post_request_malformed_json():
-    app = create_app(testing=True)
-
+def test_plate_post_request_malformed_json(app):
     with app.test_client() as test_client:
         response = test_client.post('/plate', data = {})
         assert response.status_code == 400
 
-def test_plates_page_found():
-    app = create_app(testing=True)
-
+def test_plates_page_found(app):
     with app.test_client() as test_client:
         response = test_client.get('/plates')
         assert response.status_code == 200
