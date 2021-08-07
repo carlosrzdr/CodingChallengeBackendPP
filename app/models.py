168b435da0b8082b8786fd727ba3f186b66c7aa6
Carlos Ruiz <carlos.rzdr@gmail.com>
2021-08-07 03:14:28 +0200

    [Backend] GET method enabled for /plate endpoint to retrieve all plates #8

--- app/models.py
+++ app/models.py
@@ -1,5 +1,4 @@
 from app import db
-from flask_sqlalchemy import SQLAlchemy
 import datetime
 
 class Plate(db.Model):
--- app/views.py
+++ app/views.py
@@ -10,6 +10,13 @@ ERROR_422 = json.dumps({'success':False}), 422, {'ContentType':'application/json
 
 view = Blueprint("views", __name__)
 
+def row2dict(row):
+    d = {}
+    for column in row.__table__.columns:
+        d[column.name] = str(getattr(row, column.name))
+
+    return d
+
 @view.route("/")
 def index():
     """
@@ -20,7 +27,7 @@ def index():
 @view.route("/plates")
 def plates():
     """
-    shows plates page
+    shows plates page containing the funcionalities for saving and retrieving plates
     """
     return render_template("plates.html")
 
@@ -28,6 +35,8 @@ def plates():
 def plate():
     """
     inserts a new plate into database and retrieves all plates
+    
+    :return: a json containing response codes if POST, a json containing plates data if GET
     """
     if request.method == 'POST':
         try:
@@ -44,5 +53,8 @@ def plate():
             return ERROR_422
 
     if request.method == 'GET':
-        return SUCCESS_200
-    
+        plates = Plate.query.all()
+        response = []
+        for plate in plates:
+            response.append(row2dict(plate))
+        return jsonify(response)
